336baaeb2a71b3c2982e0abab903009053d2a420
sam <sam@freighttrust.com>
2020-09-05 22:09:11 -0700

    fix: deps && dist
    Signed-off-by: sam <sam@freighttrust.com>

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
-const {
-  sha3,
-  BN
-} = require("web3-utils");
+const { sha3, BN } = require("web3-utils");
 const abiCoder = require("web3-eth-abi");
 
 const state = {
@@ -21,18 +18,12 @@ function _typeToString(input) {
 }
 
 function _addABI(abiArray) {
-
   if (Array.isArray(abiArray)) {
     // Iterate new abi to generate method id"s
     abiArray.map(function (abi) {
       if (abi.name) {
         const signature = sha3(
-          abi.name +
-          "(" +
-          abi.inputs
-            .map(_typeToString)
-            .join(",") +
-          ")"
+          abi.name + "(" + abi.inputs.map(_typeToString).join(",") + ")"
         );
         if (abi.type === "event") {
           state.methodIDs[signature.slice(2)] = abi;
@@ -55,13 +46,13 @@ function _removeABI(abiArray) {
       if (abi.name) {
         const signature = sha3(
           abi.name +
-          "(" +
-          abi.inputs
-            .map(function (input) {
-              return input.type;
-            })
-            .join(",") +
-          ")"
+            "(" +
+            abi.inputs
+              .map(function (input) {
+                return input.type;
+              })
+              .join(",") +
+            ")"
         );
         if (abi.type === "event") {
           if (state.methodIDs[signature.slice(2)]) {
@@ -105,7 +96,7 @@ function _decodeMethod(data) {
         const isArray = Array.isArray(param);
 
         if (isArray) {
-          parsedParam = param.map(val => new BN(val).toString());
+          parsedParam = param.map((val) => new BN(val).toString());
         } else {
           parsedParam = new BN(param).toString();
         }
@@ -119,7 +110,7 @@ function _decodeMethod(data) {
           if (param[0].constructor === Array) {
             parsedParam = eval(param.toString().toLowerCase());
           } else {
-            parsedParam = param.map(_ => _.toLowerCase());
+            parsedParam = param.map((_) => _.toLowerCase());
           }
         } else {
           parsedParam = param.toLowerCase();
@@ -138,79 +129,83 @@ function _decodeMethod(data) {
 }
 
 function _decodeLogs(logs) {
-  return logs.filter(log => log.topics.length > 0).map((logItem) => {
-    const methodID = logItem.topics[0].slice(2);
-    const method = state.methodIDs[methodID];
-    if (method) {
-      const logData = logItem.data;
-      let decodedParams = [];
-      let dataIndex = 0;
-      let topicsIndex = 1;
+  return logs
+    .filter((log) => log.topics.length > 0)
+    .map((logItem) => {
+      const methodID = logItem.topics[0].slice(2);
+      const method = state.methodIDs[methodID];
+      if (method) {
+        const logData = logItem.data;
+        let decodedParams = [];
+        let dataIndex = 0;
+        let topicsIndex = 1;
 
-      let dataTypes = [];
-      method.inputs.map(function (input) {
-        if (!input.indexed) {
-          dataTypes.push(input.type);
-        }
-      });
+        let dataTypes = [];
+        method.inputs.map(function (input) {
+          if (!input.indexed) {
+            dataTypes.push(input.type);
+          }
+        });
 
-      const decodedData = abiCoder.decodeParameters(
-        dataTypes,
-        logData.slice(2)
-      );
+        const decodedData = abiCoder.decodeParameters(
+          dataTypes,
+          logData.slice(2)
+        );
 
-      // Loop topic and data to get the params
-      method.inputs.map(function (param) {
-        let decodedP = {
-          name: param.name,
-          type: param.type,
-        };
+        // Loop topic and data to get the params
+        method.inputs.map(function (param) {
+          let decodedP = {
+            name: param.name,
+            type: param.type,
+          };
 
-        if (param.indexed) {
-          decodedP.value = logItem.topics[topicsIndex];
-          topicsIndex++;
-        } else {
-          decodedP.value = decodedData[dataIndex];
-          dataIndex++;
-        }
+          if (param.indexed) {
+            decodedP.value = logItem.topics[topicsIndex];
+            topicsIndex++;
+          } else {
+            decodedP.value = decodedData[dataIndex];
+            dataIndex++;
+          }
 
-        if (param.type === "address") {
-          if (decodedP.value) {
-            decodedP.value = decodedP.value.toLowerCase();
-            // 42 because len(0x) + 40
-            if (decodedP.value.length > 42) {
-              let toRemove = decodedP.value.length - 42;
-              let temp = decodedP.value.split("");
-              temp.splice(2, toRemove);
-              decodedP.value = temp.join("");
+          if (param.type === "address") {
+            if (decodedP.value) {
+              decodedP.value = decodedP.value.toLowerCase();
+              // 42 because len(0x) + 40
+              if (decodedP.value.length > 42) {
+                let toRemove = decodedP.value.length - 42;
+                let temp = decodedP.value.split("");
+                temp.splice(2, toRemove);
+                decodedP.value = temp.join("");
+              }
             }
           }
-        }
 
-        if (
-          param.type === "uint256" ||
-          param.type === "uint8" ||
-          param.type === "int"
-        ) {
-          // ensure to remove leading 0x for hex numbers
-          if (typeof decodedP.value === "string" && decodedP.value.startsWith("0x")) {
-            decodedP.value = new BN(decodedP.value.slice(2), 16).toString(10);
-          } else {
-            decodedP.value = new BN(decodedP.value).toString(10);
+          if (
+            param.type === "uint256" ||
+            param.type === "uint8" ||
+            param.type === "int"
+          ) {
+            // ensure to remove leading 0x for hex numbers
+            if (
+              typeof decodedP.value === "string" &&
+              decodedP.value.startsWith("0x")
+            ) {
+              decodedP.value = new BN(decodedP.value.slice(2), 16).toString(10);
+            } else {
+              decodedP.value = new BN(decodedP.value).toString(10);
+            }
           }
 
-        }
+          decodedParams.push(decodedP);
+        });
 
-        decodedParams.push(decodedP);
-      });
-
-      return {
-        name: method.name,
-        events: decodedParams,
-        address: logItem.address,
-      };
-    }
-  });
+        return {
+          name: method.name,
+          events: decodedParams,
+          address: logItem.address,
+        };
+      }
+    });
 }
 
 module.exports = {
